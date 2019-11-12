09b9f8b7da88ac9b703d378e50accd20bd7d0de8
Julian Tellez <juliantellezmendez@gmail.com>
2019-11-12 21:30:20 +0000

    added(utils): font styles

---
+++ utils/styles.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Colours
+color_yellow="\e[33m"
+
+# Font Styles
+font_reset="\033[0m"
+font_highlight="\033[41m\033[97m"
+font_dot="\033[31m▸ $reset"
+font_dim="\033[2m"
+font_bold=$(tput bold)
+font_normal=$(tput sgr0)
+font_underline="\e[37;4m"
