668c2d6ef8bb5534eaa6850c3afa05f92ab775c0
Julian Tellez <juliantellezmendez@gmail.com>
2019-11-13 02:33:24 +0000

    fix(step: nodejs): uses npm and avn instead

--- dependencies/nodejs.sh
+++ dependencies/nodejs.sh
@@ -3,18 +3,14 @@
 . $(pwd)/helpers/print.sh
 
 install_nodejs(){
-    if [ -x nvm ]; then
-        curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+    if ! [ -x "$(command -v node)" ]; then
+        curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
+        npm install -g avn avn-nvm avn-n
+        avn setup
 
-        print_success "NVM installed"
-        print "Installing Nodejs" ${tput_color_yellow}
-
-        nvm install stable
-        nvm use stable
-        nvm run stable --version
         nodev=$(node -v)
         print_success "Using Node $nodev!"
     else
-        print_success "NVM/Node already installed."
+        print_success "Nodejs and NVM are already installed."
     fi
 }
