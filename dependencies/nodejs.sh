e215db3a82af689382ed6f06612dfa8f18c8d6dd
Julian Tellez <juliantellezmendez@gmail.com>
2019-11-13 02:37:56 +0000

    fix(step: nodejs): installs nvm and avn on every run :(

--- dependencies/nodejs.sh
+++ dependencies/nodejs.sh
@@ -3,14 +3,10 @@
 . $(pwd)/helpers/print.sh
 
 install_nodejs(){
-    if ! [ -x "$(command -v node)" ]; then
-        curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
-        npm install -g avn avn-nvm avn-n
-        avn setup
+    curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
+    npm install -g avn avn-nvm avn-n
+    avn setup
 
-        nodev=$(node -v)
-        print_success "Using Node $nodev!"
-    else
-        print_success "Nodejs and NVM are already installed."
-    fi
+    nodev=$(node -v)
+    print_success "Using Node $nodev!"
 }
