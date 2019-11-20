9cae4d703804130be876e603d3abad9d099adc5c
Julian <juliantellezmendez@gmail.com>
2019-11-20 23:02:01 +0000

    fix(node and nvm): install latest node version

--- dependencies/nodejs.sh
+++ dependencies/nodejs.sh
@@ -3,19 +3,27 @@
 . $(pwd)/helpers/print.sh
 
 install_nodejs(){
-    local dir=$HOME/.nvm
+    local dir=$HOME/.nvm/nvm.shd
 
-    if ! [[ -d $dir ]]; then
+    if ! [[ -f $dir ]]; then
         curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+        print_success "NVM installed"
 
-        print_info "Installing latest Node"
-        nvm install node
-        nvm use node
-        nvm run node --version
-        nodev=$(node -v)
-
-        print_success "Using Node $nodev!"
+        install_node_version
     else
-        print_success "Node + NVM already installed"
+        print_success "NVM already installed"
     fi
+}
+
+install_node_version() {
+    export NVM_DIR="$HOME/.nvm"
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+
+    print_info "Installing latest Node"
+    nvm install node
+    nvm use node
+    nvm run node --version
+    node_version=$(node -v)
+
+    print_success "Node ${node_version} installed"
 }
