19d81e0cfe930be84b6eed00ab7df54c6ac2b753
Julian <juliantellezmendez@gmail.com>
2019-11-27 19:04:03 +0000

    added(github actions): test bootstrap

--- utils/prompt.sh
+++ utils/prompt.sh
@@ -1,35 +1,31 @@
 #!/bin/bash
 
+. $(pwd)/helpers/ask.sh
 . $(pwd)/helpers/print.sh
 . $(pwd)/helpers/admin_access.sh
 . $(pwd)/utils/styles.sh
-. $(pwd)/main.sh
 
-print_question "UP will download binaries and configure this machine. Are you sure? ${font_bold}(y/n)${font_reset}"
-read START
+prompt() {
+    if ask "UP will configure this machine and requires Admin access. Are you sure?" Y; then
+        # Override for development
+        if ! [ "${UP_ENV}" = "development" ]; then
+            admin_access
+        fi
 
-if ! [[ $START =~ ^[Yy]$ ]]; then
-    print_error "UP requires your consent beforehand; Exiting now. \n"
-    exit 0
-fi;
+        else
+        print_error "UP requires 'Admin Access' to be granted; Exiting now. \n"
+        exit 0
+    fi;
 
-print_question "UP requires Admin access. Are you sure? ${font_bold}(y/n)${font_reset}"
-read ADMIN_ACCESS
+    if [ -z "${GIT_USERNAME}" ]; then
+        print_question "Git username"
+        read GIT_USERNAME
+        export GIT_USERNAME
+    fi
 
-if [[ $ADMIN_ACCESS =~ ^[Yy]$ ]]; then
-    admin_access
-    else
-    print_error "UP requires 'Admin Access' to be granted; Exiting now. \n"
-    exit 0
-fi;
-
-print_question "git username"
-read GIT_USERNAME
-
-print_question "git email"
-read GIT_EMAIL
-
-export GIT_USERNAME
-export GIT_EMAIL
-
-main
+    if [ -z "${GIT_EMAIL}" ]; then
+        print_question "Git email"
+        read GIT_EMAIL
+        export GIT_EMAIL
+    fi
+}
