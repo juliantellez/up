3b4f0dfbc270fa7b3bff8411a6d176247deab7ff
Julian <juliantellezmendez@gmail.com>
2019-11-27 19:26:53 +0000

    added(github actions): test install script

--- install.sh
+++ install.sh
@@ -4,7 +4,8 @@
 # This script should be run via:
 # sh -c "$(curl -fsSL https://raw.githubusercontent.com/juliantellez/up/master/install.sh)"
 #
+GITHUB_SHA={1:master}
 
 mkdir -p ~/.up; cd ~/.up; \
-curl -L https://github.com/juliantellez/up/tarball/master | tar -xzv --strip-components 1 &> /dev/null; \
+curl -L "https://github.com/juliantellez/up/tarball/${GITHUB_SHA}" | tar -xzv --strip-components 1 &> /dev/null; \
 sh bootstrap.sh
